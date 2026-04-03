bf68c8c11e14562ac7ed148230ec68a7159e6c15
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-03 23:46:46 +0200

    add header

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,48 +1,39 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
 import "../Styles/header.css";
 
-export default function Header({ user }){
+export default function Header({ user, menu, setMenu, onLogout }) {
+  return (
+    <header className="header">
 
-  const navigate = useNavigate();
-
-  const logout = () => {
-
-    localStorage.removeItem("access");
-    localStorage.removeItem("refresh");
-
-    navigate("/");
-
-  };
-
-  return(
-
-    <div className="main-header">
-
-      <div className="logo">
-        StaffHub
-      </div>
-
-      <div className="main-menu">
-
-        <Link to="/dossiers">Dossiers</Link>
-
-        <Link to="/activites">Activités</Link>
-
-      </div>
-
-      <div className="header-user">
-
-        Bonjour {user?.prenom} {user?.nom}
-
-        <button className="logout-btn" onClick={logout}>
-          Déconnexion
+      {/* GAUCHE */}
+      <div className="header-left">
+        <button
+          className={menu === "dossier" ? "nav-btn active" : "nav-btn"}
+          onClick={() => setMenu("dossier")}
+        >
+          Dossier
         </button>
 
+        <button
+          className={menu === "activites" ? "nav-btn active" : "nav-btn"}
+          onClick={() => setMenu("activites")}
+        >
+          Activité
+        </button>
       </div>
 
-    </div>
+      {/* DROITE */}
+      <div className="header-right">
+        <p className="user-text">
+          Vous êtes connecté en tant que <strong>Agent</strong>
+          {user && ` (${user.prenom} ${user.nom})`}
+        </p>
 
+        <button className="logout-btn" onClick={onLogout}>
+          Déconnexion
+        </button>
+      </div>
+
+    </header>
   );
-
 }
