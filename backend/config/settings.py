c19f1de97cd3417da3717fc001cc745e0f9d8388
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-09-21 01:00:55 +0200

    add setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,7 +144,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "apps.users.authentication.CookieJWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
