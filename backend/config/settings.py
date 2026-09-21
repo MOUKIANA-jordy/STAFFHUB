060bd7dfe2702764226efe907316cf5ea28bd991
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-09-21 16:28:59 +0200

    add security_access

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ROTATE_REFRESH_TOKENS": True,
